de48df69c71aceab5a5fbf562187ee38f2c340c9
Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-02 11:56:58 +0100

    🧮 - Add conversion

--- src/main/java/me/loule/decimaltobinary/controller/MainController.java
+++ src/main/java/me/loule/decimaltobinary/controller/MainController.java
@@ -1,14 +1,58 @@
 package me.loule.decimaltobinary.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-public class MainController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextField textFieldDecimal;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textFieldBinary;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("MainController initialized");
+
+        textFieldDecimal.setOnKeyReleased(event -> {
+            textFieldBinary.setText(convertDecimalToBinary(Integer.parseInt(textFieldDecimal.getText())));
+        });
+
+        textFieldBinary.setOnKeyReleased(event -> {
+            textFieldDecimal.setText(convertBinaryToDecimal(Integer.parseInt(textFieldBinary.getText())));
+        });
+    }
+
+    public String convertDecimalToBinary(int num) {
+        ArrayList<String> binary = new ArrayList<>();
+
+        String result = "";
+
+        while (num > 0) {
+            binary.add(String.valueOf(num % 2));
+            num = num / 2;
+        }
+
+        for (int i = binary.size() - 1; i >= 0; i--) {
+            result += binary.get(i);
+        }
+
+        return result;
+    }
+
+    public String convertBinaryToDecimal(int num) {
+        int result = 0;
+
+        for (int i = 0; num != 0; i++) {
+            result += (num % 10) * Math.pow(2, i);
+            num = num / 10;
+        }
+
+        return Integer.toString(result);
     }
 }
